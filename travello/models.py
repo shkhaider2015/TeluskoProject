4f85c25b9f234aeec6851b72c8497d2ab6a06fc1
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-10-14 23:25:47 +0500

    Database Register

--- travello/models.py
+++ travello/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    id : int
-    name : str
-    desc : str
-    price : int
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='pics')
+    offer = models.BooleanField(default=False)
+    # django model field
