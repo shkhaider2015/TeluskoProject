92babdfc00921076bb18ae2fdce948cb4e34997c
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-10-16 15:56:54 +0500

    dict key ERROR

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_name = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password1']
+        password2 = request.POST['password2']
+
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=user_name, email=email, password=password1)
+        user.save()
+        print('user created')
+        return redirect('/')
+
+    else:
+        return render(request, 'register.html')
