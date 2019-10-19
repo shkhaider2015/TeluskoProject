299fee1b6aa87e1b3b5b2fb870a5da89d8193df3
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-10-19 16:08:50 +0500

    Add static files of login template and succefully run login method

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,25 @@ from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
+def login(request):
+    if request.method == 'POST':
+        user_name = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username= user_name, password=password)
+
+        if user is not None:
+            print("User is not None")
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            print("User is  None ???")
+            messages.info(request, "Invalid Credential")
+            return redirect("login")
+    else:
+        return render(request, 'login.html')
+
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['firstname']
@@ -27,7 +46,7 @@ def register(request):
                 user = User.objects.create_user(first_name=first_name, last_name=last_name, username=user_name, email=email, password=password)
                 user.save()
                 print('user created')
-                return redirect('/')
+                return redirect('login')
         else:
             print("Password is not matching")
             messages.info(request, 'Password is not matching')
